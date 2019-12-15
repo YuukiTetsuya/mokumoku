9e5bdb16e62b59868ef06c080fb56fec541fe256
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-12-16 03:44:02 +0900

    ドキュメントルートにアクセス時、webroot/index,phpをインクルードする

---
+++ index.php
@@ -0,0 +1,1 @@
+<?= require_once 'webroot/index.php';
--- webroot/index.php
+++ webroot/index.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>MokuMokuAppへようこそ</title>
 </head>
 
 <body>
-    <p>test</p>
+    <h1>このサイトは、もくもく会進行を手助けします</h1>
     <?= print "{$_SERVER['DOCUMENT_ROOT']}/mokumoku/webroot/index.php"; ?>
 </body>
 
