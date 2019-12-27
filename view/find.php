a03ac77908b7257fc3eb2fd3bb1b7e38e97f6b23
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-12-27 15:50:47 +0900

    検索画面入力値空のバリデーションをstorageにも追加 flashメッセージのセッション保存箇所をfindに移動

--- view/find.php
+++ view/find.php
@@ -6,6 +6,11 @@ if (!isset($_POST['token']) || $_POST['token'] !== $_SESSION['token']) {
     die('不正なアクセスが行われました');
 };
 
+// 検索時のみ検索件数をお知らせするメッセージを追加
+if (isset($_SESSION['token'])) {
+    $_SESSION['finded'] = 'true';
+};
+
 // 検索キーワードが送信されているかどうか確認。なければ空を代入
 if (isset($_POST['search'])) {
     $keyword = $_POST['search'];
